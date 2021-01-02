cc94589bb1abff913474db2e746a6a13b3f7668a
NineAsher <516888847@qq.com>
2021-01-02 23:03:32 +0800

    First version commit
    I added two elements:
    RGBVideo
    RectLedMatrix

--- src/main/java/de/neemann/digital/plugin/NALibSource.java
+++ src/main/java/de/neemann/digital/plugin/NALibSource.java
@@ -10,7 +10,7 @@ import de.neemann.digital.gui.Main;
 /**
  * Adds some components to Digital
  */
-public class NALibComponentSource implements ComponentSource {
+public class NALibSource implements ComponentSource {
 
     /**
      * Attach your components to the simulator by calling the add methods
@@ -20,10 +20,10 @@ public class NALibComponentSource implements ComponentSource {
      */
     @Override
     public void registerComponents(ComponentManager manager) throws InvalidNodeException {
-
+        manager.addComponent("my folder/my sub folder", MyAnd.DESCRIPTION);
         /*
         // add a component and use the default shape
-        manager.addComponent("my folder/my sub folder", MyAnd.DESCRIPTION);
+        
 
         // add a component and also provide a custom shape
         manager.addComponent("my folder/my sub folder", MyOr.DESCRIPTION, MyOrShape::new);
@@ -35,7 +35,6 @@ public class NALibComponentSource implements ComponentSource {
         manager.addComponent("my folder/RAM", MultiPortRAM.DESCRIPTION,
                 (attr, inputs, outputs) ->
                         new GenericShape("RAM", inputs, outputs, attr.getLabel(), true, 5));
-        
         */
 
         manager.addComponent("NALib/IO", RectLedMatrix.DESCRIPTION);
@@ -51,7 +50,7 @@ public class NALibComponentSource implements ComponentSource {
      */
     public static void main(String[] args) {
         new Main.MainBuilder()
-                .setLibrary(new ElementLibrary().registerComponentSource(new NALibComponentSource()))
+                .setLibrary(new ElementLibrary().registerComponentSource(new NALibSource()))
                 .openLater();
     }
 }
